91dcfd30ad3d0119af5700434a8cb8e706b9dc00
Abraham14711 <74873275+Abraham14711@users.noreply.github.com>
2022-03-30 12:26:42 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,39 +4,6 @@ app = Flask(__name__)
 
 @app.route('/mainpage')
 def main_page():
-    try:
-        if request.method=='post':
-            import re
-            import urllib.parse
-            import urllib.request
-            import bs4
-            def getReport(myurl):
-                try:
-                    reponse = urllib.request.Request(myurl)
-                    html = urllib.request.urlopen(reponse).read().decode("utf-8")
-                    soup = bs4.BeautifulSoup(html, 'html.parser')
-                    pattern = r'http://v.virscan.org/\w+'
-                    vLinks = soup.find_all('a', href=re.compile(pattern))
-                    if len(vLinks) == 0:
-                        return 'safe'
-                    else:
-                        for vlink in vLinks:
-                            url3 = urllib.parse.quote(vlink['href'])
-                            url3 = url3.replace('http%3A', 'http:')
-                            if vlink.has_attr('alt'):
-                                return 'Unsafe'
-                            else:
-                                return 'safe'
-                except:
-                    return'Unsafe'
-
-            if getReport(request.form.get('url'))=='safe':
-                message='Это безопасный сайт'
-                return render_template('mainpage.html', message=message)
-            else:
-                message='Сайт не безопасен'
-                return render_template('mainpage.html',message=message)
-    except:pass
     return render_template('mainpage.html')
 
 @app.route('/vulnerabilities')
