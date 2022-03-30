8f8cb0f0a05aba1542c2f5be95e9912da5118d04
Abraham14711 <74873275+Abraham14711@users.noreply.github.com>
2022-03-30 14:30:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,41 +2,41 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/',m)
+@app.route('/',methods=['POST','GET'])
 def main_page():
-    return render_template('mainpage.html')
+    if request.method=='POST':
+        import re
+        import urllib.parse
+        import urllib.request
+        import bs4
+        def getReport(myurl):
+            try:
+                reponse = urllib.request.Request(myurl)
+                html = urllib.request.urlopen(reponse).read().decode("utf-8")
+                soup = bs4.BeautifulSoup(html, 'html.parser')
+                pattern = r'http://v.virscan.org/\w+'
+                vLinks = soup.find_all('a', href=re.compile(pattern))
+                if len(vLinks) == 0:
+                    return 'safe'
+                else:
+                    for vlink in vLinks:
+                        url3 = urllib.parse.quote(vlink['href'])
+                        url3 = url3.replace('http%3A', 'http:')
+                        if vlink.has_attr('alt'):
+                            return 'Unsafe'
+                        else:
+                            return 'safe'
+            except:
+                return 'Unsafe'
 
-
-    import re
-    import urllib.parse
-    import urllib.request
-    import bs4
-    def getReport(myurl):
-        try:
-            reponse = urllib.request.Request(myurl)
-            html = urllib.request.urlopen(reponse).read().decode("utf-8")
-            soup = bs4.BeautifulSoup(html, 'html.parser')
-            pattern = r'http://v.virscan.org/\w+'
-            vLinks = soup.find_all('a', href=re.compile(pattern))
-            if len(vLinks) == 0:
-                return 'safe'
-            else:
-                for vlink in vLinks:
-                    url3 = urllib.parse.quote(vlink['href'])
-                    url3 = url3.replace('http%3A', 'http:')
-                    if vlink.has_attr('alt'):
-                        return 'Unsafe'
-                    else:
-                        return 'safe'
-        except:
-            return 'Unsafe'
-
-    if getReport(request.form.get('url')) == 'safe':
-        message = 'Это безопасный сайт'
-        return render_template('mainpage.html', message=message)
+        if getReport(request.form.get('url')) == 'safe':
+            message = 'Это безопасный сайт'
+            return render_template('mainpage.html', message=message)
+        else:
+            message = 'Сайт не безопасен'
+            return render_template('mainpage.html', message=message)
     else:
-        message = 'Сайт не безопасен'
-        return render_template('mainpage.html', message=message)
+        return render_template('mainpage.html')
 
 @app.route('/vulnerabilities')
 def contacts():
@@ -48,4 +48,3 @@ def new_things():
 
 if __name__ == '__main__':
     app.run()
-
